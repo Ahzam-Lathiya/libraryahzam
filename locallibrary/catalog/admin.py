b9f4515f38045f3f9cd82372bbf251229f9f4002
ahzam lathiya <ahzam.lathiya@gmail.com>
2020-05-15 17:57:05 +0500

    initial commit

---
+++ locallibrary/catalog/admin.py
@@ -0,0 +1,42 @@
+from django.contrib import admin
+from .models import Author, Genre, Book, BookInstance
+
+# Register your models here.
+
+admin.site.register(Genre)
+
+class BooksInstanceInline(admin.TabularInline):
+    model = BookInstance
+
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
+
+    fields = ['first_name', 'last_name', 'date_of_birth', 'date_of_death']
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+
+    list_display = ('title', 'author')
+    inlines = [BooksInstanceInline]
+    
+    def display_genre(self):
+        """Create a string for the Genre. This is required to display genre in Admin."""
+        return ', '.join(genre.name for genre in self.genre.all()[:3])
+    
+    display_genre.short_description = 'Genre'
+
+@admin.register(BookInstance)
+class BookInstanceAdmin(admin.ModelAdmin):
+    list_display = ('book', 'status', 'borrower', 'due_back', 'id')
+    list_filter = ('status', 'due_back')
+    
+    fieldsets = (
+        (None, {
+            'fields' : ('book', 'imprint', 'id')
+        }),
+        ('Availability', {
+            'fields': ('status', 'due_back', 'borrower')
+        }),
+    )
+
+admin.site.register(Author, AuthorAdmin)
